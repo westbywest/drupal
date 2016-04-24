f5c0388b811ab21e6d4010daee14f13eef9ade85
Ben West <me@benwest.name>
2016-04-24 13:54:00 -0500

    upgrading mailchimp module

--- sites/all/modules/contrib/mailchimp/mailchimp.api.php
+++ sites/all/modules/contrib/mailchimp/mailchimp.api.php
@@ -6,73 +6,15 @@
  */
 
 /**
- * Respond to an email being added to a list.
+ * Alter mergevars before they are sent to MailChimp.
  *
- * @param $list
- *   MailChimp list object.
- * @param $email
- * @param $merge_vars
+ * @param array $mergevars
+ * @param object $entity
+ * @param string $entity_type
+ *
+ * @return NULL
  */
-function hook_mailchimp_subscribe_user($list, $email, $merge_vars) {
-
-}
-
-/**
- * Respond to an email being removed from a list.
- *
- * @param $list
- *   MailChimp list object.
- * @param $email
- */
-function hook_mailchimp_unsubscribe_user($list, $email) {
-
-}
-
-/**
- * Return an array of additional merge tokens.
- *
- * @return array
- */
-function hook_mailchimp_lists_merge_tokens() {
-  $out = array('' => t('-- Select --'));
-
-  // invoke hook to get all merge tokens
-  $tokens = module_invoke_all('mailchimp_lists_merge_tokens');
-
-  foreach ($tokens as $key => $token) {
-    $out[$key] = t('!field', array('!field' => $token['name']));
-  }
-
-  return $out;
-}
-
-/**
- * Return an array of matching merge values.
- *
- * @param $mergevars
- * @param $account
- * @param $list
- *
- * @return array
- */
-function hook_mailchimp_lists_merge_values($mergevars, $account, $list) {
-  $values = array();
-
-  // grab the saved list merge vars and filter out unset values
-  if (!empty($list->settings['mergefields'])) {
-    $mergevars = array_filter($list->settings['mergefields']);
-    $mergevars = array_flip($mergevars);
-
-    // match with token values
-    $values = module_invoke_all('mailchimp_lists_merge_values', $mergevars, $account, $list);
-
-    // always add email
-    $values += array(
-      'EMAIL' => $account->mail
-    );
-  }
-
-  return $values;
+function hook_mailchimp_lists_mergevars_alter(&$mergevars, $entity, $entity_type) {
 }
 
 /**
@@ -86,5 +28,31 @@ function hook_mailchimp_lists_merge_values($mergevars, $account, $list) {
  *   The data contained in the webhook.
  */
 function hook_mailchimp_process_webhook($type, $data) {
+
+}
+
+/**
+ * Perform an action after a subscriber has been subscribed.
+ *
+ * @string $list_id
+ *   MailChimp list id.
+ * @string $email
+ *   Subscriber email address.
+ * @array $merge_vars
+ *   Submitted user values.
+ */
+function hook_mailchimp_subscribe_user($list_id, $email, $merge_vars) {
+
+}
+
+/**
+ * Perform an action after a subscriber has been unsubscribed.
+ *
+ * @string $list_id
+ *   MailChimp list id.
+ * @string $email
+ *   Subscriber email address.
+ */
+function hook_mailchimp_unsubscribe_user($list_id, $email) {
 
 }
