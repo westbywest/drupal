47b0199189b2f094993861b2238925d15c89d0c1
Ben West <me@benwest.name>
2019-12-01 12:49:32 -0600

    media_gallery to 7.x-2.x-dev with patches

--- sites/all/modules/dg_modified/media_gallery/colorbox-display.js
+++ sites/all/modules/dg_modified/media_gallery/colorbox-display.js
@@ -4,7 +4,8 @@ Drupal.behaviors.mediaGalleryColorbox.attach = function (context, settings) {
   var $ = jQuery, $galleries, $gallery, href, $links, $link, $dummyLinksPre, $dummyLinksPost, i, j;
   if ($.fn.colorbox) {
     // Add a colorbox group for each media gallery field on the page.
-    $galleries = $('.field-name-media-gallery-media');
+    // Second selector is to cover BEM syntax which some themes like Omega uses.
+    $galleries = $('.field-name-media-gallery-file, .field--name-media-gallery-file');
     for (i = 0; i < $galleries.length; i++) {
       $gallery = $($galleries[i]);
       $links = $('a.cbEnabled', $gallery);
